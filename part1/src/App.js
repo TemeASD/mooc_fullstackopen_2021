9c08a21420f375118550f92b31c171a18b6a370f
TemeASD <temesus@hotmail.com>
2021-03-29 23:14:57 +0300

    exc1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -22,26 +22,29 @@ const Part = (props) => {
   return <p>{props.part} {props.exercises}</p>
 }
 const App = () => {
-  const course = 'Half Stack application development'
 
-  const parts = [{
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
+  const course =
   {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
+    name: 'Half Stack application development',
+    parts: [{
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }]
   }
-]
+
   return (
     <div>
-      <Header course={course} />
-      <Content part={parts}  />
-      <Total part={parts}   />
+      <Header course={course.name} />
+      <Content part={course.parts} />
+      <Total part={course.parts} />
     </div>
   )
 }
